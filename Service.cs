0b00a3b9fe45edfb46a428c3f16c08c6d15cad2d
Ganesh Daund <daundganesh496@gmail.com>
2021-06-14 10:43:49 +0530

    review/DI

---
+++ Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dependency_Injection
+{
+    public class Service: ISingletonService, IScopedService, ITransientService
+    {
+        Guid id;
+        public Service()
+        {
+            id = Guid.NewGuid();
+        }
+
+        public Guid GetID()
+        {
+            return id;
+        }
+    }
+}
